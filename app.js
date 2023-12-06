f02cccdf266fa671275b12a340d7e1e3260cc48e
Yuan-ueng <yuanyueheng2019@163.com>
2023-12-05 16:12:08 -0800

    fix uri

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: 'https://a6--jazzy-entremet-1fd456.netlify.app'
+    origin: 'https://a6--offer4y.netlify.app'
   })
 );
 
